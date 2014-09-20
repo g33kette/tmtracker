9b01c946d8b57078e0973bb170b96fdd1e435101
Jo Buxton <jo@g33kette.co.uk>
2014-09-20 18:14:09 +0100

    twitter working woo

--- js/feeds/twitter.js
+++ js/feeds/twitter.js
@@ -1,31 +1,34 @@
 // Twitter fetch code lives in here
 var tw = new Twitter();
-tw.stream('hack');
+tw.stream('kitten');
 function Twitter(){
     var fs = require('fs'),
         util = require('util'),
         twitter = require('twitter'),
-        config = JSON.parse(fs.readFileSync('config/twitter.json'));
+        config = JSON.parse(fs.readFileSync('config/twitter.json')),
+        mongo = require('../output/mongodb.js');
 
     var twit = new twitter(config);
 
     this.stream = function(filter){
         twit.stream('statuses/filter', {'track' : filter}, function(stream) {
             stream.on('data', function(data) {
-                //console.log(util.inspect(data));
-                //console.log(data.text);
                 save(data);
-                stream.destroy();
+                //stream.destroy();
             });
-            //setTimeout(stream.destroy, 5000);
         });
 
     };
 
     var save = function(tweet){
-        var url = 'https://twitter.com/'+tweet.user.screen_name+'/status/'+tweet.id_str;
-        var text = tweet.text;
-        console.log(url);
-        console.log(text);
+        var data = {
+            'url' : 'https://twitter.com/'+tweet.user.screen_name+'/status/'+tweet.id_str,
+            'text' : tweet.text
+        };
+        mongo.connect(function(){
+            mongo.save(data);
+            console.log(data);
+            mongo.close();
+        });
     };
 }
