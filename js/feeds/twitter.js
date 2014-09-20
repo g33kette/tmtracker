4c92053ab31643038d6a917155695d3a0af8a9d3
Jo Buxton <jo@g33kette.co.uk>
2014-09-20 17:45:28 +0100

    twitter feed search for hack

--- js/feeds/twitter.js
+++ js/feeds/twitter.js
@@ -1,1 +1,28 @@
 // Twitter fetch code lives in here
+var tw = new Twitter();
+tw.stream();
+function Twitter() {
+
+    var fs = require('fs'),
+        util = require('util'),
+        twitter = require('twitter'),
+        config = JSON.parse(fs.readFileSync('config/twitter.json'));
+
+    var twit = new twitter(config);
+
+    this.stream = function(){
+        twit.stream('statuses/filter', {'track' : 'hack'}, function(stream) {
+            stream.on('data', function(data) {
+                //console.log(util.inspect(data));
+                console.log(data.text);
+                //stream.destroy();
+            });
+            //setTimeout(stream.destroy, 5000);
+        });
+
+    };
+
+    var save = function(){
+
+    };
+}
